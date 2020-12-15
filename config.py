77f9c4f4ab7f03c63fedfabcfeabd75929a08247
nadineuwineza <nadineuwineza2017@gmail.com>
2020-12-15 09:35:50 +0200

    added api

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
 
-    
+    QUOTE_API_BASE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://nadine:moringa123@localhost/blogs'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
